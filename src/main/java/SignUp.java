289fbf355b066c0e20067c75e8b61ff4817bc03b
Alex Contra <contraalex8@gmail.com>
2020-04-29 16:54:20 +0300

    Created Sign Up class

---
+++ src/main/java/SignUp.java
@@ -0,0 +1,10 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SignUp implements ActionListener
+{
+
+    public void actionPerformed(ActionEvent actionEvent) {
+
+    }
+}
